6453fdd057798e24f45df622a0191524de8888a1
TSUYUSATO Kitsune <make.just.on@gmail.com>
2017-05-17 17:20:33 +0900

    Implement AST matching

--- src/main/scala/AST.scala
+++ src/main/scala/AST.scala
@@ -1,7 +1,64 @@
 package bfa
 
+import scala.annotation.tailrec
+
 sealed abstract class AST {
   import AST._
+
+  def matches(s: String): Boolean = matches(Reader(s)) exists { _.eof }
+
+  private def matches(re: Reader): Option[Reader] = this match {
+    case Alt(l, r)    => matchesAlt(re, l, r)
+    case Concat(l, r) => matchesConcat(re, l, r)
+
+    case PositiveLookAhead(n)  => matchesPositiveLookAhead(re, n)
+    case NegativeLookAhead(n)  => matchesNegativeLookAhead(re, n)
+    case PositiveLookBehind(n) => matchesPositiveLookBehind(re, n)
+    case NegativeLookBehind(n) => matchesNegativeLookBehind(re, n)
+
+    case Star(n)  => matchesStar(re, n)
+    case Plus(n)  => matchesPlus(re, n)
+    case Quest(n) => matchesQuest(re, n)
+
+    case Literal(c) => matchesLiteral(re, c)
+    case Empty      => matchesEmpty(re)
+  }
+
+  private[this] def matchesAlt(re: Reader, l: AST, r: AST) =
+    l.matches(re) orElse r.matches(re)
+
+  private[this] def matchesConcat(re: Reader, l: AST, r: AST) =
+    l.matches(re) flatMap { r.matches(_) }
+
+  private[this] def matchesPositiveLookAhead(re: Reader, n: AST) =
+    n.matches(re.forward) map { _ => re }
+
+  private[this] def matchesNegativeLookAhead(re: Reader, n: AST) =
+    n.matches(re.forward) orElse Some(re)
+
+  private[this] def matchesPositiveLookBehind(re: Reader, n: AST) =
+    n.reverse.matches(re.backward) map { _ => re }
+
+  private[this] def matchesNegativeLookBehind(re: Reader, n: AST) =
+    n.reverse.matches(re.backward) orElse Some(re)
+
+  @tailrec
+  private[this] def matchesStar(re: Reader, n: AST): Option[Reader] =
+    n.matches(re) match {
+      case Some(re) => matchesStar(re, n)
+      case None     => Some(re)
+    }
+
+  private[this] def matchesPlus(re: Reader, n: AST) =
+    n.matches(re) flatMap { matchesStar(_, n) }
+
+  private[this] def matchesQuest(re: Reader, n: AST) =
+    n.matches(re) orElse Some(re)
+
+  private[this] def matchesLiteral(re: Reader, c: Char) =
+    if (re.current == Some(c)) Some(re.next) else None
+
+  private[this] def matchesEmpty(re: Reader) = Some(re)
 
   def reverse: AST = this match {
     case Alt(l, r)    => Alt(l.reverse, r.reverse)
