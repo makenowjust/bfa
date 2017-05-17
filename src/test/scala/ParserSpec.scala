f9afaeb0f47656a5979cad83fd6e4f2bc0b373c7
TSUYUSATO Kitsune <make.just.on@gmail.com>
2017-05-17 15:21:16 +0900

    Add tests for parser

---
+++ src/test/scala/ParserSpec.scala
@@ -0,0 +1,39 @@
+package bfa
+
+import org.scalatest._
+
+class ParserSpec extends WordSpec with MustMatchers {
+  import RegexAST._
+
+  "Parser.parse" should {
+    List[(String, RegexAST)](
+      ("", Empty),
+      ("f", Literal('f')),
+      ("fo", Concat(Literal('f'), Literal('o'))),
+      ("foo", Concat(Concat(Literal('f'), Literal('o')), Literal('o'))),
+      ("f|o", Alt(Literal('f'), Literal('o'))),
+      ("f|o|o", Alt(Alt(Literal('f'), Literal('o')), Literal('o'))),
+      ("f|o|o|", Alt(Alt(Alt(Literal('f'), Literal('o')), Literal('o')), Empty)),
+      ("f*", Star(Literal('f'))),
+      ("f+", Plus(Literal('f'))),
+      ("f?", Quest(Literal('f'))),
+      ("a(?=b)", Concat(Literal('a'), PositiveLookAhead(Literal('b')))),
+      ("a(?!b)", Concat(Literal('a'), NegativeLookAhead(Literal('b')))),
+      ("(?<=a)b", Concat(PositiveLookBehind(Literal('a')), Literal('b'))),
+      ("(?<!a)b", Concat(NegativeLookBehind(Literal('a')), Literal('b')))
+    ).foreach { case (s, t) =>
+      s"""parses "${s}"""" in {
+        Parser.parse(s) must be(Some(t))
+      }
+    }
+
+    List(
+      "*", "+", "?",
+      "(", ")", ")("
+    ).foreach { s =>
+      s"""not parses "${s}"""" in {
+        Parser.parse(s) must be(None)
+      }
+    }
+  }
+}
