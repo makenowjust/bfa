8ec1299c10a112b835e7ad0305c69518bc0dc550
TSUYUSATO Kitsune <make.just.on@gmail.com>
2017-05-17 17:19:08 +0900

    Implement Reader for AST matching

---
+++ src/main/scala/Reader.scala
@@ -0,0 +1,32 @@
+package bfa
+
+trait Reader {
+  def current: Option[Char]
+
+  def eof: Boolean = current == None
+
+  def next: Reader
+
+  def forward: Reader
+
+  def backward: Reader
+}
+
+object Reader {
+  def apply(s: String): Reader = ReaderImpl(s, 0, true)
+
+  private case class ReaderImpl(text: String, offset: Int, forwarding: Boolean) extends Reader {
+    def current: Option[Char] =
+      if (0 <= index && index < text.length) Some(text(index)) else None
+
+    def next: Reader = ReaderImpl(text, offset + direction, forwarding)
+
+    private[this] def index = if (forwarding) offset else offset - 1
+
+    private[this] def direction = if (forwarding) 1 else -1
+
+    def forward: Reader = ReaderImpl(text, offset, true)
+
+    def backward: Reader = ReaderImpl(text, offset, false)
+  }
+}
---
+++ src/test/scala/ReaderSpec.scala
@@ -0,0 +1,41 @@
+package bfa
+
+import org.scalatest._
+
+class ReaderSpec extends WordSpec with MustMatchers {
+  "eof" must {
+    "check eof if true" in {
+      Reader("").eof must be(true)
+    }
+
+    "check eof not if true" in {
+      Reader("foo").eof must not be(true)
+    }
+  }
+
+  "current and next" must {
+    "work fine" in {
+      val re1 = Reader("abc")
+      re1.current must be(Some('a'))
+      val re2 = re1.next
+      re2.current must be(Some('b'))
+      val re3 = re2.next
+      re3.current must be(Some('c'))
+      val re4 = re3.next
+      re4.current must be(None)
+    }
+  }
+
+  "forward and backward" must {
+    "work fine" in {
+      val re1 = Reader("abc").backward
+      re1.current must be(None)
+      val re2 = re1.forward
+      re2.current must be(Some('a'))
+      val re3 = re2.next.next.next.backward.next
+      re3.current must be(Some('b'))
+      val re4 = re3.forward
+      re4.current must be(Some('c'))
+    }
+  }
+}
