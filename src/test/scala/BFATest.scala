95449ea30cb48c5972e0c8b6744d1bc31489e104
TSUYUSATO Kitsune <make.just.on@gmail.com>
2017-08-03 11:46:06 +0900

    Add tests for BFA#matches

---
+++ src/test/scala/BFATest.scala
@@ -0,0 +1,28 @@
+package bfa
+
+import org.scalatest.{WordSpec, MustMatchers}
+
+class BFATest extends WordSpec with MustMatchers {
+  import DNF._
+
+  val A = DNF.symbol('A)
+  val B = DNF.symbol('B)
+  val C = DNF.symbol('C)
+  val D = DNF.symbol('D)
+  val `¬A` = DNF.not('A)
+  val `¬B` = DNF.not('B)
+  val `¬C` = DNF.not('C)
+  val `¬D` = DNF.not('D)
+
+  "matches" must {
+    "match an empty string against an empty BFA" in {
+      val bfa = BFA(DNF(A), Map.empty, Set('A))
+      bfa.matches("") must be(true)
+    }
+
+    "match a string against a simple BFA" in {
+      val bfa = BFA(DNF(A), Map(('A, 'a') -> DNF(B)), Set('B))
+      bfa.matches("a") must be(true)
+    }
+  }
+}
