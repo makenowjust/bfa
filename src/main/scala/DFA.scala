687e1de183ac87c1d453bec17e118311e8b6ba70
TSUYUSATO Kitsune <make.just.on@gmail.com>
2017-10-10 17:12:29 +0900

    Implement minimization

--- src/main/scala/DFA.scala
+++ src/main/scala/DFA.scala
@@ -54,6 +54,52 @@ final case class DFA(
       .reduceLeftOption(_ | _)
       .getOrElse(Fail)
   }
+
+  def minimize: DFA = {
+    val vs = symbols
+    val cs = trans.iterator.map { case ((_, c), _) => c }.toSet
+
+    val dead = Symbol("d")
+
+    def delta2(q: Symbol, p: Symbol, c: Char): (Symbol, Symbol) = {
+      val q1 = trans.getOrElse((q, c), dead)
+      val p1 = trans.getOrElse((p, c), dead)
+      (q1, p1)
+    }
+
+    val d0 = for {
+      q <- last
+      p <- (vs + dead) diff last
+    } yield (q, p)
+
+    val `di+1`: Set[(Symbol, Symbol)] => Set[(Symbol, Symbol)] = {
+      di => di | (for {
+        q <- symbols
+        p <- symbols
+        if cs.exists { c => di.contains(delta2(q, p, c)) }
+      } yield (q, p))
+    }
+
+    lazy val di: Stream[Set[(Symbol, Symbol)]] = d0 #:: di.map(`di+1`)
+    val d = (di zip di.tail).dropWhile { case ((di, di1)) => di != di1 }.head._1
+
+    def indi(q: Symbol): Set[Symbol] = vs.filter { p => !d.contains((q, p)) }
+
+    var id = 0
+    val qMap = new mutable.HashMap[Set[Symbol], Symbol]
+    def indi2q(indi: Set[Symbol]): Symbol = qMap.getOrElseUpdate(indi, { id += 1; Symbol(s"v$id")})
+
+    val i = indi2q(indi(init))
+    val t = vs.foldLeft((Set.empty[Symbol], Map.empty[(Symbol, Char), Symbol])) {
+      case ((done, t), v) =>
+        val q = indi2q(indi(v))
+        if (done.contains(q)) (done, t)
+        else (done + q, t ++ cs.iterator.map { c => trans.get((v, c)).map { p => (q, c) -> indi2q(indi(p)) }.toList }.flatten.toMap)
+    }._2
+    val l = last.map { q => indi2q(indi(q)) }
+
+    DFA(i, t, l)
+  }
 }
 
 object DFA {
